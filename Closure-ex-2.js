af23c839337b41b2f59e2565e333e319d1bdb055
Paul Ren <pause.forever@gmail.com>
2022-09-14 13:52:34 +0700

    add answers in closure exercises

--- Closure-ex-2.js
+++ Closure-ex-2.js
@@ -1,15 +1,29 @@
 function matchAction(x) {
 // Start coding here
+function addByX(y) {
+    return y + x;
 }
+function subtractByX(z) {
+    return z - x;
+}
+return {
+    addByX,
+    subtractByX
+}
+}
+
 
 // Uncomment โค้ดด้านล่างเพื่อดูผลลัพธ์ของการทำงาน
 
-// const addByTwo = matchAction(2);
-// console.log(addByTwo.addByX(1)); // => should return 3
-// console.log(addByTwo.addByX(2)); // => should return 4
-// console.log(addByTwo.addByX(3)); // => should return 5
+const addByTwo = matchAction(2);
+console.log(addByTwo.addByX(1)); // => should return 3
+console.log(addByTwo.addByX(2)); // => should return 4
+console.log(addByTwo.addByX(3)); // => should return 5
 
-// const subtractByThree = matchAction(3);
-// console.log(subtractByThree.subtractByX(4)); // => should return 1
-// console.log(subtractByThree.subtractByX(6)); // => should return 3
-// console.log(subtractByThree.subtractByX(8)); // => should return 5
+const subtractByThree = matchAction(3);
+console.log(subtractByThree.subtractByX(4)); // => should return 1
+console.log(subtractByThree.subtractByX(6)); // => should return 3
+console.log(subtractByThree.subtractByX(8)); // => should return 5
+
+
+// เรื่อง Closure ก็เป็นอีกเรื่องที่ไม่ค่อยเข้าใจ
